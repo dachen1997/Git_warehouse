04e4508d71d3aabc7f3a0d3774f84544770a2cdd
DaChen <1414922017@qq.com>
2021-04-28 09:07:29 +0800

    delete NewScriptWrappingExercises

--- NewScriptWrappingExercises/Config/configData.py
+++
@@ -1,7 +0,0 @@
-URL = "http://47.115.122.235:9003"
-sqlhost = "192.168.241.47"
-sqlport = 3306
-sqluser = "pzservice"
-sqlpasswd = "pzservice"
-sqldatabase = "pzservice"
-
--- NewScriptWrappingExercises/all.py
+++
@@ -1,8 +0,0 @@
-
-import pytest
-import os
-from Tools.Handler_Pathconfig import TmpDir_PATH,Report_PATH
-
-if __name__ == '__main__':
-    pytest.main()
-    os.system(f'allure generate {TmpDir_PATH} -o {Report_PATH} --clean')
